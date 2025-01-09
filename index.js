c085e3d18c56059ada691aff63835049fcf8d66e
fabioferrero90 <fabio.ff90@gmail.com>
2025-01-09 13:01:51 +0100

    feat: add errorhandlers in middleware

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 const postsRouter = require('./routers/posts.js');
+const errorsHandler = require('./middlewares/errorshandler');
+const notFound = require('./middlewares/notFound');
 
 const port = 3000;
 
 app.use(express.json());
-app.use('/posts', postsRouter)
+
+app.use('/posts', postsRouter);
+
+app.use(errorsHandler);
+
+app.use(notFound);
+
 
 app.listen(port, () => {
-  console.log(`Sto ascoltando alla porta ${port}`)
+  console.log(`Sto ascoltando alla porta ${port}`);
 })
 
 app.get('/', (req, res) => {
-  res.send('Il mio Blog')
+  res.send('Il mio Blog');
 })
